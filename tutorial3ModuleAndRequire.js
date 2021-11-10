3360e56d8e798a107d9bce92045b2c02289a077c
robinrrr10 <robinrrr10@gmail.com>
2021-11-10 16:07:17 +0530

    added module and require logic

---
+++ tutorial3ModuleAndRequire.js
@@ -0,0 +1,3 @@
+var counter = require('./tutorial3SampleFile'); //file name. no need of .js //This is used to import
+
+console.log(counter([34, 23, 12, 44]))
---
+++ tutorial3SampleFile.js
@@ -0,0 +1,5 @@
+var count = function(arr){
+  return 'length is: ' + arr.length;
+};
+
+module.exports = count;      //This is used export count  function outside this file as well
