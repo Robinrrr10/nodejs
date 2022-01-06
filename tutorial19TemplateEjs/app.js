a13460d63446db6fef41e7b4afaeb5ea780acdae
robinrrr10 <robinrrr10@gmail.com>
2022-01-06 22:44:07 +0530

    added template ejs and sample

---
+++ tutorial19TemplateEjs/app.js
@@ -0,0 +1,28 @@
+var express = require('express')
+
+var app = express();
+
+app.set('view engine', 'ejs') //to add ejs   //Also add views folder and keep all ejs template file there
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/index.html")
+})
+
+app.get('/about', function(req, res){
+  res.send('about page')
+})
+
+app.get('/contact', function(req, res){
+  res.sendFile(__dirname + "/contact.html")
+})
+
+app.get('/profile/:name', function(req, res){
+  res.send('profile name is:' + req.params.name)
+})
+
+app.get('/profileUser/:name', function(req, res){
+  var data = {age: 24, dept: 'cse'};
+  res.render('user', {username: req.params.name, userdata: data}) //user.ejs file is available inside views. so we have user      //Here we use render and pass param and objects //It will be used in html using username and userdata
+})
+
+app.listen(3000)
